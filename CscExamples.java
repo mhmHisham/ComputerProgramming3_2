6bd14aff6b431f00af43520373585c9bf27b0dd7
mhmHisham <cscuser@cssamba.jfn.ac.lk>
2020-09-18 04:22:06 +0530

    impplemented the CscExamples.java

---
+++ CscExamples.java
@@ -0,0 +1,26 @@
+public class CscExamples{
+	private int quId
+	
+	public  addQution(CscExample quId){
+		
+	}
+	
+	public void getQustionNo(){
+		
+	}
+	
+	public void getCatogory(){
+		
+	}
+	public void gettext(){
+		
+	}
+	
+	public  removeQutions(int quId){
+		
+	}
+	public editQution(int quID){
+		
+	}
+
+}
