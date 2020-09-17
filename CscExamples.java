723fd73d281ad75e404e44ad38ece3cd5ae414fe
mhmHisham <cscuser@cssamba.jfn.ac.lk>
2020-09-18 04:28:17 +0530

    also implemented the CscExamples.java

---
+++ CscExamples.java
@@ -0,0 +1,15 @@
+
+
+
+public class CscExamples{
+
+	private int questionNo;
+	private String catogory;
+	private String text;
+
+	public setQuestionNo(){};
+	public setCatogory(){};
+	public setText(){};
+}
+
+
